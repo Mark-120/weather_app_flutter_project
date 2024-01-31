69497972f3d1b35d4358366906f9bda4421ca121
mark-120 <78369499+Mark-120@users.noreply.github.com>
2024-02-01 00:08:35 +0300

    Added the ability to search the weather in different countries (Added the search screen and connected it to the main screen)

--- lib/screens/weather_forecast_screen.dart
+++ lib/screens/weather_forecast_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app_flutter_project/api/weather_api.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:weather_app_flutter_project/screens/city_screen.dart';
 import 'package:weather_app_flutter_project/widgets/bottom_listview.dart';
 import 'package:weather_app_flutter_project/widgets/city_view.dart';
 import 'package:weather_app_flutter_project/widgets/temp_view.dart';
@@ -33,42 +34,67 @@ class _WeatherForecastScreenState extends State<WeatherForecastScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      backgroundColor: Colors.black,
-      title: Text('Weather Forecast', style: TextStyle(color: Colors.white),),
-      centerTitle: true,
-      leading: IconButton(onPressed: () {}, icon: Icon(Icons.my_location, color: Colors.white)),
-      actions: <Widget>[
-        IconButton(onPressed: () {}, icon: Icon(Icons.location_city, color: Colors.white))
-      ],
-    ),
-    body: ListView(
-      children: [
-        Container(
-          child: FutureBuilder<WeatherForecast>(future: forecastObject, builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return Column(
-                children: [
-                  SizedBox(height: 50,),
-                  CityView(snapshot: snapshot),
-                  SizedBox(height: 50),
-                  TempView(snapshot: snapshot),
-                  SizedBox(height: 50),
-                  DetailView(snapshot: snapshot),
-                  SizedBox(height: 50),
-                  BottomListView(snapshot: snapshot),
-                ],
-              );
-            }
-            else {
-              return const Center(
-                child: SpinKitDoubleBounce(color: Colors.black87, size: 100,),
-              );
-            }
-          }),
-        )
-      ],
-    ),
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text(
+          'Weather Forecast',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        leading: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.my_location, color: Colors.white)),
+        actions: <Widget>[
+          IconButton(
+              onPressed: () async {
+                var tappedName = await Navigator.push(context,
+                    MaterialPageRoute(builder: (context) {
+                  return CityScreen();
+                }));
+                setState(() {
+                  if (tappedName != null && tappedName.length != 0) {
+                    _cityName = tappedName;
+                    forecastObject = WeatherApi()
+                        .fetchWeatherForecastWithCity(cityName: _cityName);
+                  }
+                });
+              },
+              icon: Icon(Icons.location_city, color: Colors.white))
+        ],
+      ),
+      body: ListView(
+        children: [
+          Container(
+            child: FutureBuilder<WeatherForecast>(
+                future: forecastObject,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Column(
+                      children: [
+                        SizedBox(
+                          height: 50,
+                        ),
+                        CityView(snapshot: snapshot),
+                        SizedBox(height: 50),
+                        TempView(snapshot: snapshot),
+                        SizedBox(height: 50),
+                        DetailView(snapshot: snapshot),
+                        SizedBox(height: 50),
+                        BottomListView(snapshot: snapshot),
+                      ],
+                    );
+                  } else {
+                    return const Center(
+                      child: SpinKitDoubleBounce(
+                        color: Colors.black87,
+                        size: 100,
+                      ),
+                    );
+                  }
+                }),
+          )
+        ],
+      ),
     );
   }
 }
