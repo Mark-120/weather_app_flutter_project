3048ce1a9cfaceefca688b1af23ba058bda11780
mark-120 <78369499+Mark-120@users.noreply.github.com>
2024-02-01 01:46:01 +0300

    Removed Debug Banner

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: LocationScreen(),
+      debugShowCheckedModeBanner: false
     );
   }
 }
